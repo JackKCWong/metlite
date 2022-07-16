6f7cdd24c87e0c0050d11a01d172929fa16de6cf
Jack HUANG <jiajue.huang@live.com>
2022-07-16 20:35:41 +0800

    add memstat

--- main.go
+++ main.go
@@ -41,6 +41,12 @@ type cpuCoreStat struct {
 	CpuStatID uint
 }
 
+type memStat struct {
+	ID        uint `gorm:"primaryKey"`
+	CreatedAt time.Time
+	Stat      procfs.Meminfo `gorm:"embedded"`
+}
+
 func main() {
 	fs, err := procfs.NewDefaultFS()
 	if err != nil {
@@ -52,10 +58,9 @@ func main() {
 		log.Panicf("failed to open test.db: %q", err)
 	}
 
-	db.AutoMigrate(&cpuStat{}, &cpuCoreStat{})
+	db.AutoMigrate(&cpuStat{}, &cpuCoreStat{}, &memStat{})
 
 	sigCh := make(chan os.Signal, 1)
-
 	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
 
 	for {
@@ -64,40 +69,61 @@ func main() {
 			log.Printf("exit")
 			os.Exit(0)
 		default:
-			var rec cpuStat
-			stat, err := fs.Stat()
+			cpu, err := getCpuStat(fs)
 			if err != nil {
-				log.Printf("stat err: %q", err)
+				log.Printf("cpu stat err: %q", err)
 			}
 
-			rec.BootTime = stat.BootTime
-			rec.Stat = stat.CPUTotal
-			rec.ContextSwitches = stat.ContextSwitches
-			rec.ProcessCreated = stat.ProcessCreated
-			rec.ProcessesBlocked = stat.ProcessesBlocked
-			rec.ProcessesRunning = stat.ProcessesRunning
-			rec.IRQTotal = stat.IRQTotal
-			rec.SoftIRQTotal = stat.SoftIRQTotal
-
-			cpus := make([]cpuCoreStat, 0, len(stat.CPU))
-			for i := range stat.CPU {
-				var cpu cpuCoreStat
-				cpu.Stat = stat.CPU[i]
-				cpu.NO = uint(i)
-				cpus = append(cpus, cpu)
-				log.Printf("%+v", cpu)
-			}
-
-			rec.CPUs = cpus
-
-			result := db.Create(&rec)
+			result := db.Create(&cpu)
 			if result.Error != nil {
-				log.Printf("create err: %q", result.Error)
+				log.Printf("cpu stat save err: %q", result.Error)
 			}
 
-			log.Printf("%+v", rec)
+			var mem memStat
+			meminfo, err := fs.Meminfo()
+			if err != nil {
+				log.Printf("mem stat err: %q", err)
+			}
 
+			mem.Stat = meminfo
+
+			result = db.Create(&mem)
+			if result.Error != nil {
+				log.Printf("mem stat save err: %q", result.Error)
+			}
+
+			log.Printf("%+v", cpu)
+			log.Printf("%+v", mem)
 			time.Sleep(1 * time.Second)
 		}
 	}
+}
+
+func getCpuStat(fs procfs.FS) (cpuStat, error) {
+	var rec cpuStat
+	stat, err := fs.Stat()
+	if err != nil {
+		return cpuStat{}, err
+	}
+
+	rec.BootTime = stat.BootTime
+	rec.Stat = stat.CPUTotal
+	rec.ContextSwitches = stat.ContextSwitches
+	rec.ProcessCreated = stat.ProcessCreated
+	rec.ProcessesBlocked = stat.ProcessesBlocked
+	rec.ProcessesRunning = stat.ProcessesRunning
+	rec.IRQTotal = stat.IRQTotal
+	rec.SoftIRQTotal = stat.SoftIRQTotal
+
+	cpus := make([]cpuCoreStat, 0, len(stat.CPU))
+	for i := range stat.CPU {
+		var cpu cpuCoreStat
+		cpu.Stat = stat.CPU[i]
+		cpu.NO = uint(i)
+		cpus = append(cpus, cpu)
+	}
+
+	rec.CPUs = cpus
+
+	return rec, nil
 }
